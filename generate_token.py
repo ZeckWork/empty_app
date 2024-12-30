e59089ba826a9a8234f3381e32860be83a9f1676
Ezequiel <me@zeck.work>
2024-12-30 07:04:42 -0300

    ci:distribute iOS

---
+++ generate_token.py
@@ -0,0 +1,31 @@
+import jwt
+import time
+
+def generate_token(key_file_path, team_id, key_id):
+    with open(key_file_path, "r") as key_file:
+        private_key = key_file.read()
+
+    payload = {
+        "iss": team_id,
+        "iat": int(time.time()),
+        "exp": int(time.time()) + 20 * 60,
+        "aud": "appstoreconnect-v1"
+    }
+
+    headers = {
+        "kid": key_id
+    }
+
+    token = jwt.encode(payload, private_key, algorithm="ES256", headers=headers)
+    return token
+
+
+if __name__ == "__main__":
+    import os
+
+    key_file_path = "AuthKey.p8"
+    team_id = os.environ["ISSUER_ID"]
+    key_id = os.environ["KEY_ID"]
+
+    token = generate_token(key_file_path, team_id, key_id)
+    print(token)
